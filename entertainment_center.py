85f338dbfaf7abfa77db5ca3f19576b633153b18
rubalcava <radgokart@gmail.com>
2016-09-09 16:27:10 -0700

    feat: Updated code to adhere to PEP8 feat: Added comments explaining code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,54 @@
+# The next two lines import other python files so we can
+# use their functionality
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie('Toy Story',
-                        'A story of a boy and his toys that come to life', 'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg', 'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+# The next six statements are instantiations of the Movie class
+toy_story = media.Movie(
+    'Toy Story',
+    'A story of a boy and his toys that come to life',
+    'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',  # noqa
+    'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
-avatar = media.Movie('Avatar',
-                     'A marine on an alien planet',
-                     'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
-                     'https://www.youtube.com/watch?v=aVdO-cx-McA')
+avatar = media.Movie(
+    'Avatar',
+    'A marine on an alien planet',
+    'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',  # noqa
+    'https://www.youtube.com/watch?v=aVdO-cx-McA')
 
-lion_king = media.Movie('The Lion King',
-                     'Embark on an extraordinary coming-of-age adventure as Simba, a lion cub who cannot wait to be king',
-                     'https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg',
-                     'https://www.youtube.com/watch?v=GaJWzJfoXlE')
+lion_king = media.Movie(
+    'The Lion King',
+    'Embark on an extraordinary coming-of-age adventure'
+    ' as Simba, a lion cub who cannot wait to be king',
+    'https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg',  # noqa
+    'https://www.youtube.com/watch?v=GaJWzJfoXlE')
 
-aladdin = media.Movie('Aladdin',
-                     'When street rat Aladdin frees a genie from a lamp, he finds his wishes granted.',
-                     'https://upload.wikimedia.org/wikipedia/en/5/58/Aladdinposter.jpg',
-                     'https://www.youtube.com/watch?v=QapaqcDucmg')
+aladdin = media.Movie(
+    'Aladdin',
+    'When street rat Aladdin frees a genie from a lamp,'
+    ' he finds his wishes granted.',
+    'https://upload.wikimedia.org/wikipedia/en/5/58/Aladdinposter.jpg',
+    'https://www.youtube.com/watch?v=QapaqcDucmg')
 
-beauty_beast = media.Movie('Beauty and the Beast',
-                     "An arrogant young prince and his castle's servants fall under the spell of a wicked enchantress, who turns him into the hideous Beast until he learns to love and be loved in return.",
-                     'https://upload.wikimedia.org/wikipedia/en/7/7c/Beautybeastposter.jpg',
-                     'https://www.youtube.com/watch?v=tRlzmyveDHE')
+beauty_beast = media.Movie(
+    'Beauty and the Beast',
+    "An arrogant young prince and his castle's servants"
+    " fall under the spell of a wicked enchantress, who"
+    "turns him into the hideous Beast until he learns to"
+    "love and be loved in return.",
+    'https://upload.wikimedia.org/wikipedia/en/7/7c/Beautybeastposter.jpg',  # noqa
+    'https://www.youtube.com/watch?v=tRlzmyveDHE')
 
-little_mermaid = media.Movie('The Little Mermaid',
-                     "A rebellious 16-year-old mermaid Ariel is fascinated with life on land",
-                     'https://upload.wikimedia.org/wikipedia/en/7/75/Movie_poster_the_little_mermaid.jpg',
-                     'https://www.youtube.com/watch?v=ZGZX5-PAwR8')
+little_mermaid = media.Movie(
+    'The Little Mermaid',
+    "A rebellious 16-year-old mermaid Ariel is"
+    " fascinated with life on land",
+    'https://upload.wikimedia.org/wikipedia/en/7/75/Movie_poster_the_little_mermaid.jpg',  # noqa
+    'https://www.youtube.com/watch?v=ZGZX5-PAwR8')
 
+# This puts the objects in a list
 movies = [toy_story, avatar, lion_king, aladdin, beauty_beast, little_mermaid]
 
+# This calls a function from the imported fresh_tomatoes file and
+# passes movies as an argument.This is what will render the page.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,20 @@
 import webbrowser
 
+
 class Movie():
     '''This class provides a way to store movie information'''
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    # This function constructs the class with variables
+    # for instantiated objects of the class
+    def __init__(
+        self, movie_title, movie_storyline, poster_image,
+            trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # This function is a class function and available to
+    # instantiated objects of the class
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
